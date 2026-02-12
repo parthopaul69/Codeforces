583692cf9e08a6cf6b8709dce2549dc1c5459fd6
Partho <10legendgaming@gmail.com>
2026-02-13 03:26:34 +0600

    Add Codeforces 231A - Team

--- 800/231A.java
+++ 800/231A.java
@@ -3,13 +3,23 @@ class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int count=0;
-        for(int i=0;i<n;i++)
+        int[][] y=new int[n][3];
+        for(int i=0;i<y.length;i++)
         {
-            int x=sc.nextInt();
-            int y=sc.nextInt();
-            int z=sc.nextInt();
-            if(x+y+z>=2)
+            for(int j=0;j<y[i].length;j++)
+            {
+                y[i][j]=sc.nextInt();
+            }
+        }
+        int sum,count=0;
+        for(int i=0;i<y.length;i++)
+        {
+            sum=0;
+            for(int j=0;j<y[i].length;j++)
+            {
+                sum=sum+y[i][j];
+            }
+            if(sum>1)
             {
                 count++;
             }
