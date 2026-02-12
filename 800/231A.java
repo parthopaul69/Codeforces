bb6051c7c308213894640715fdb6e411e9c454d9
Partho <10legendgaming@gmail.com>
2026-02-13 03:20:22 +0600

    Add Codeforces 231A - Team

---
+++ 800/231A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if(x+y+z>=2)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
