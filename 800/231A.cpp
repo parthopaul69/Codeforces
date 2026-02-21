0c7401e4eee3fa2253da90890ae92c9089b4128c
Partho <10legendgaming@gmail.com>
2026-02-22 04:55:06 +0600

    sync

---
+++ 800/231A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,count=0;
+    cin>>n;
+    while(n!=0)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c>=2)
+        {
+            count++;
+        }
+        n--;
+    }
+    cout<<count;
+    return 0;
+}
