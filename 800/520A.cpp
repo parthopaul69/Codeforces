4ea09937093c34ec60fbd4b08140f75c5c762df0
Partho <10legendgaming@gmail.com>
2026-02-28 02:29:44 +0600

    sync

---
+++ 800/520A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    string s;
+    cin>>n>>s;
+    set<char>st;
+    for(char c:s)
+    {
+        if(c>='A' && c<='Z')
+        {
+            c=tolower(c);
+        }
+        st.insert(c);
+    }
+    if(st.size()==26)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
